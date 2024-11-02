01a30528a9bde008a184628dd376798250781393
Daniel Levi <daniellevi000@gmail.com>
2024-11-02 12:56:42 -0300

    feat: 1- add nav bar 2-create layout

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from "react";
+import { NavBar } from "../components/navbar";
+
+export default function Layout({children}: {children: ReactNode}){
+  return (
+    <>
+    <NavBar />
+
+        {children}
+ 
+    </>
+  )
+}
