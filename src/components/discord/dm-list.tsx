99e8358e27247ca21fcd29e4dcabe7cc230628ca
Daniel Levi <daniellevi000@gmail.com>
2024-11-02 15:47:52 -0300

    wip: Add DM list content and styling to Discord UI clone

--- src/components/discord/dm-list.tsx
+++ src/components/discord/dm-list.tsx
@@ -1,3 +1,6 @@
+import { Inbox, UserCircle } from "lucide-react";
+import Image from "next/image";
+
 export function DmList() {
   return (
     <div className="hidden md:flex w-60 bg-[#2F3136] flex-col">
@@ -6,6 +9,48 @@ export function DmList() {
           Find or Start a conversation
         </div>
       </div>
+      
+      <div className="flex-1 overflow-y-auto pt-4">
+        <div className="px-2 mb-4">
+          <div className="flex items-center text-sm px-2 py-1.5 rounded hover:bg-[#393c43] text-[#dcdde} cursor-not-allowed">
+             <UserCircle className="size-8 mr-4 text-[#b9bbb3]" />
+             <span className="font-medium text-sm">Friends</span>
+          </div>
+          <div className="flex items-center text-sm px-2 py-1.5 rounded hover:bg-[#393c43] text-[#dcdde} cursor-not-allowed">
+             <Inbox className="size-8 mr-4 text-[#b9bbb3]" />
+             <span className="font-medium text-sm">Nitro</span>
+          </div>
+        </div>
+
+        <div className="px-2 mb-4">
+          <h3 className="text-xs font-semibold text-[#8E92997] px-2 mb-2 uppercase">
+            Direct Messages
+          </h3>
+          <div className="flex items-center px-2 py-1.5 rounded bg-[#393C43] text-white cursor-pointer">
+            <Image 
+              src="/brand-asset-profile-picture.png" 
+              alt="Ping Panda Avatar" 
+              width={32}
+              height={32}
+              className="object-cover rounded-full mr-3"
+            /> 
+            <span className="font-medium">PingPanda</span>
+          </div>
+          <div className="my-1 space-y-px ">
+            {[...Array(4)].map((_,index) => (
+              <div 
+                key={index}
+                className="flex items-center px-2 py-1.5 rounded text-gray-600 cursor-not-allowed"
+              >
+                <div className="size-8 rounded-full bg-discord-background mr-3" />
+                <span className="font-medium">User {index + 1}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="p-2 bg-[#292V2F] flex items-center"></div>
     </div>
   )
 }
